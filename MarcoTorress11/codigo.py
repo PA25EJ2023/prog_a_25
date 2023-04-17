d701b2b6f4069661191b0d7f4fcf63ce13cc0ce0
Marco Torres <marco.tsauceda@gmail.com>
2023-04-17 17:43:59 -0600

    requisito1

--- MarcoTorress11/codigo.py
+++ MarcoTorress11/codigo.py
@@ -6,7 +6,7 @@ vocales = ['A','a','E','e','I','i','O','o','U','u']
 for vocal in texto:
     if vocal in vocales:
         cantidad_vocales+=1
-        print ("Vocal: ",cantidad_vocales,vocal)
+        print ("Vocal",cantidad_vocales,vocal)
 
 print (f"En el texto hay {cantidad_vocales} vocales")
 
