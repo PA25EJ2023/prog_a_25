ec40316f2c973e98ff548b1932b90cf3c515a4bb
Ricardo Enrique Morton Sosa <rickysosa486@outlook.com>
2023-04-24 16:25:49 -0600

    Subir archivos

---
+++ rickymorton/noob.py
@@ -0,0 +1,52 @@
+import math
+
+print("Bienvenido")
+print("Opciones disponibles")
+print("1.cuadrado\n 2.Triangulo\n 3.Circulo")
+opcion = int(input("Elige una opcion [1,2,3]"))
+
+if opcion == 1: 
+    print("Calculo disponible")
+    print("1.Area\n 2.Perimetro\n 3.Diagonal")
+    calculo = int(input("Elige una opcion"))
+
+    lado = float(input("Ingresa el valor del lado"))
+    if calculo == 1:
+        area = lado * lado
+        print(f'El area es {area}')
+    elif calculo == 2:
+        perimetro = lado * 4
+        print(f'El perimetro es {perimetro}')
+    elif calculo == 3:
+        diagonal = lado * math.sqrt(2)
+        print(f'La diagonal es {diagonal}')
+elif opcion == 2:
+    print("Calculo disponible")
+    print("1.Area\n 2.Perimetro")
+    calculo = int(input("Elige una opcion"))
+
+    base = float(input("Ingresa la base"))
+    altura = float(input("Ingresa la altura"))
+
+    if calculo == 1:
+        area = (base * altura)/2
+        print(f'El area es {area}')
+    elif calculo == 2:
+        perimetro = math.sqrt(base**2 + altura**2)
+        print(f'El perimetro es {perimetro}')
+elif opcion == 3:
+    print("Calculo disponible")
+    print("1.Area\n 2.Perimetro")
+    calculo = int(input("Elige una opcion"))
+
+    radio = float(input("Ingresa el radio"))
+
+    if calculo == 1:
+        area = math.pi * radio ** 2
+        print(f'El area es {area}')
+    elif calculo == 2:
+        perimetro = 2 * math.pi * radio
+        print(f'El perimetro es {perimetro}')
+
+
+
