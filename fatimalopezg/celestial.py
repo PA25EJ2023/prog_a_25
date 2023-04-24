0486e502271992ed2423c6ed2f14a40c86b80f7f
Fatima Alejandra Lopez Guzman <fatima.lopezgzm@uanl.edu.mx>
2023-04-24 17:38:39 -0500

    ultima modificacion nivel god

---
+++ fatimalopezg/celestial.py
@@ -0,0 +1,59 @@
+import math
+from figuras import Cuadrado,Triangulo,Circulo
+#CLASES
+
+print("Bienvenido")
+print ("Opciones disponibles")
+print ("1.Cuadrado\n2.Triangulo rectangulo\n3.Circulo")
+opcion=int(input("Elige una opcion [1,2,3]"))  
+
+if opcion == 1:
+    print("Calculos disponibles")
+    print("1.Area\n2.Perimetro\n3.Diagonal")
+    calculo = int(input("Elige una opcion"))
+
+    lado = float(input("Ingresa el valor del lado"))
+    
+    #crear un cuadrado con el lado especificado
+    cuadrado=Cuadrado(lado)  #objeto que se llama cuadrado, es una creacion de objetos
+    if calculo == 1:
+        area = cuadrado.area()       
+        print(f"El area es {area}")
+
+    elif calculo == 2:
+        perimetro = cuadrado.perimetro()
+        print(f"El perimetro es {perimetro}")
+
+    elif calculo == 3:
+        diagonal = cuadrado.diagonal()
+        print(f"La diagonal es {diagonal}")
+
+elif opcion == 2:
+    print("Calculos disponibles")
+    print("1.-Area\n2.Perimetro")
+    calculo = int(input("Elige una opcion"))
+
+    base = float(input("Ingresa la base"))
+    altura = float(input("Ingresa la altura"))
+    
+    triangulo=Triangulo(base,altura)
+    if calculo == 1:
+        area = triangulo.area()
+        print(f"El area es {area}")
+    elif calculo == 2:
+        perimetro = triangulo.perimetro()
+        print(f"El perimetro es {perimetro}")
+
+elif opcion == 3:
+    print("Calculos disponibles")
+    print("1.-Area\n2.Perimetro")
+    calculo = int(input("Elige una opcion"))
+
+    radio = float(input("Ingresa el radio"))
+    circulo=Circulo(radio)
+    if calculo == 1:
+        area = circulo.area()
+        print(f"El area es {area}")
+    elif calculo == 2:
+        perimetro = circulo.perimetro()
+        print(f"El perimetro es {perimetro}")
