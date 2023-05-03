fc70afe9e4e1f634a16fccbcdac75c24d89e2fde
Jonathan Ivan Perez Jasso <ivan0410jpj@gmail.com>
2023-05-03 18:44:55 -0500

    Agregue clases libreta, espejo, pegamento, marco

--- JonathanIPJ/creacion_impresion_objetos_f1.py
+++ JonathanIPJ/creacion_impresion_objetos_f1.py
@@ -1,4 +1,13 @@
 from clasesfoto1 import Pluma,Tijera
 
 pluma1 = Pluma("Rojo",10,"5cm","VIC")
-pluma1.imprimir
+pluma1.imprimir()
+pluma2 = Pluma("Azul",10,"7cm","VIC")
+pluma2.imprimir()
+pluma3 = Pluma("Verde",10,"7cm","VIC")
+pluma3.imprimir()
+
+tijera1 = Tijera("Azul",15,25,"Scotch")
+tijera1.imprimir()
+
+
