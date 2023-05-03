47d6d52e6b2e0174c20a960a2ca50f50e6e2fc87
Ricardo Enrique Morton Sosa <rickysosa486@outlook.com>
2023-05-03 17:42:00 -0600

    Agregamos clase Boligrafo,Espejo,Recuadro

---
+++ rickymorton/creacion_impresion_objetos_f1.py
@@ -0,0 +1,31 @@
+from clasesfoto1 import Boligrafo, Espejo, Recuadro
+
+boligrafo1 = Boligrafo("BIC","Azul",20)
+print("Boligrafo 1")
+boligrafo1.info()
+
+boligrafo2 = Boligrafo("BIC","Rosa",20)
+print("Boligrafo 2")
+boligrafo2.info()
+
+boligrafo3 = Boligrafo("BIC","Amarillo",20)
+print("Boligrafo 3")
+boligrafo2.info()
+
+boligrafo4 = Boligrafo("BIC","Naranja",20)
+print("Boligrafo 4")
+boligrafo2.info()
+
+espejo = Espejo("Pequeño","Corazon","Rojo")
+print("Espejo")
+espejo.info()
+
+recuadro1 = Recuadro("Rectangulo","Con estrellitas","Chico","Blanco")
+print("Recuadro 1")
+recuadro1.info()
+
+recuadro2 = Recuadro("Rectangulo","Luna y estrellas","Chico","Blanco")
+print("Recuadro 2")
+recuadro2.info()
+
+
