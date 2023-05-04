3310ddbbe59a4ae22c39f9af6a4912093fbdda36
FerChapa <fernando.chapaort@uanl.edu.mx>
2023-05-03 20:52:35 -0600

    agregue clases

---
+++ FerChapa0/creacion_impresion_objetos_f1.py
@@ -0,0 +1,134 @@
+from clasesfoto1 import Cuenta, Boligrafo,Pegamento,Pintura_uñas,Plumon,Poster,Espejo,Hoja_sticker,Cinta,Telefono,Tijera,Libreta,Cuca,Retrato
+#CUENTAS
+""" cuenta1=Cuenta("rosa", 1,.5, "circular")
+cuenta1.imprimir_info()
+print('*'*15)
+cuenta2=Cuenta("amarilla", 1,.5, "circular")
+cuenta2.imprimir_info()
+print('*'*15)
+cuenta3=Cuenta("celeste", 1,.5, "circular")
+cuenta3.imprimir_info()
+print('*'*15)
+cuenta4=Cuenta("naranja", 1,.5, "circular")
+cuenta4.imprimir_info()
+print('*'*15)
+cuenta5=Cuenta("blanca", 1,.5, "circular")
+cuenta5.imprimir_info()
+print('*'*15) """
+#BOLIGRAFOS
+""" boligrafo1=Boligrafo("rosa","bic",12,"circular")
+boligrafo1.imprimir_info()
+print('*'*15)
+boligrafo2=Boligrafo("azul","bic",12,"circular")
+boligrafo2.imprimir_info()
+print('*'*15)
+boligrafo3=Boligrafo("amarillo","bic",12,"circular")
+boligrafo3.imprimir_info()
+print('*'*15)
+boligrafo4=Boligrafo("rosa","peper mate",8,"triangular")
+boligrafo4.imprimir_info()
+print('*'*15)
+boligrafo5=Boligrafo("naraja","azor",17,"con mota")
+boligrafo5.imprimir_info()
+print('*'*15) 
+boligrafo6=Boligrafo("rojo","bic",12,"circular")
+boligrafo6.imprimir_info() 
+print('*'*15)
+"""
+#PLUMON
+""" plumon1=Plumon("rosa","crayola",25)
+plumon1.imprimir_info()
+print('*'*15)
+plumon2=Plumon("morado","crayola",25)
+plumon2.imprimir_info()
+print('*'*15)
+plumon3=Plumon("amarillo","crayola",25)
+plumon3.imprimir_info()
+print('*'*15) """
+#TIJERAS
+""" tijera1=Tijera("azul","escolar",8)
+tijera1.imprimir_info() """
+#LIBRETAS
+""" libreta1=Libreta("celeste",15,12,"cosida")
+libreta1.mostrar_info()
+print('*'*15)
+libreta2=Libreta("rosa",30,15,"resorte")
+libreta2.mostrar_info()
+print('*'*15)
+libreta3=Libreta("morada",20,15,"resorte grueso")
+libreta3.mostrar_info()
+print('*'*15) """
+#CINTAS
+""" cinta1=Cinta("azul", 2, "1 metro")
+cinta1.mostrar_info()
+print('*'*15)
+cinta2=Cinta("rosa", 2, "1 metro")
+cinta2.mostrar_info()
+print('*'*15)
+cinta3=Cinta("naranja", 2, "1 metro")
+cinta3.mostrar_info()
+print('*'*15)
+cinta4=Cinta("verde", 2, "1 metro")
+cinta4.mostrar_info()
+print('*'*15) """
+#ESPEJO
+""" espejo1=Espejo("rosa","corazon",225,25)
+espejo1.mostrar_info() """
+#PINTURA UÑAS
+""" esmalte1=Pintura_uñas("celeste","circular",50, 3)
+esmalte1.mostrar_info()
+print('*'*15)
+esmalte2=Pintura_uñas("blanco","circular",25, 5)
+esmalte2.mostrar_info()
+print('*'*15)
+esmalte3=Pintura_uñas("rosa brillos","ovalo",40, 3)
+esmalte3.mostrar_info()
+print('*'*15)
+esmalte4=Pintura_uñas("rosa pastel","cuadrado",50, 3)
+esmalte4.mostrar_info()
+print('*'*15)
+esmalte5=Pintura_uñas("rosa","diamante",35, 5)
+esmalte5.mostrar_info()
+print('*'*15)
+esmalte6=Pintura_uñas("rojo","circular plano",50, 3)
+esmalte6.mostrar_info()
+print('*'*15)
+esmalte7=Pintura_uñas("brillos","circular",50, 3)
+esmalte7.mostrar_info()
+print('*'*15) """
+#PEGAMENTO
+""" pegamento1=Pegamento("rosa","cilindro", 25, "mediano",)
+pegamento1.mostrar_info() """
+#RETRATO
+""" retrato1=Retrato("beige",25,15,"estrellas doradas")
+retrato1.mostrar_info()
+print('*'*15)
+retrato2=Retrato("blanco",25,15,"flores rosas")
+retrato2.mostrar_info() """
+#TELEFONO
+""" telefono1=Telefono("rosa","analogico",250,"1 kilogramo")
+telefono1.mostrar_info() 
+print('*'*15)"""
+#STICKER
+""" hoja1=Hoja_sticker("gris, morado, azul", "cilindros",25)
+hoja1.mostrar_info()
+print('*'*15)
+hoja2=Hoja_sticker("celeste, plateado, morado", "bolitas",25)
+hoja2.mostrar_info()
+print('*'*15) """
+#POSTER
+""" poster1=Poster("celeste", "rectangular", "rombos")
+poster1.mostrar_info
+print('*'*15)
+poster2=Poster("morado", "rectangular", "letras amarillas")
+poster2.mostrar_info
+print('*'*15)
+poster3=Poster("colores", "cuadrado", "foto de un arbol")
+poster3.mostrar_info
+print('*'*15)
+poster4=Poster("colores", "rectangular", "foto de la playa")
+poster4.mostrar_info
+print('*'*15) """
+#CUCAS
+cuca1=Cuca("celeste", "pequeña","pinza")
+cuca1.mostrar_info()
