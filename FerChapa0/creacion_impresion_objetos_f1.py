d305de690c492ce55a76f821f3633cbdcf80c158
FerChapa <fernando.chapaort@uanl.edu.mx>
2023-05-04 08:59:02 -0600

    termine creacion de objetos f1 y agruegue clase labiales

--- FerChapa0/creacion_impresion_objetos_f1.py
+++ FerChapa0/creacion_impresion_objetos_f1.py
@@ -1,6 +1,7 @@
-from clasesfoto1 import Cuenta, Boligrafo,Pegamento,Pintura_uñas,Plumon,Poster,Espejo,Hoja_sticker,Cinta,Telefono,Tijera,Libreta,Cuca,Retrato
+from clasesfoto1 import Cuenta,Pinta_Labios, Boligrafo,Pegamento,Pintura_uñas,Plumon,Poster,Espejo,Hoja_sticker,Cinta,Telefono,Tijera,Libreta,Cuca,Retrato
 #CUENTAS
-""" cuenta1=Cuenta("rosa", 1,.5, "circular")
+print("CUENTAS")
+cuenta1=Cuenta("rosa", 1,.5, "circular")
 cuenta1.imprimir_info()
 print('*'*15)
 cuenta2=Cuenta("amarilla", 1,.5, "circular")
@@ -14,9 +15,10 @@ cuenta4.imprimir_info()
 print('*'*15)
 cuenta5=Cuenta("blanca", 1,.5, "circular")
 cuenta5.imprimir_info()
-print('*'*15) """
+print('*'*15)
 #BOLIGRAFOS
-""" boligrafo1=Boligrafo("rosa","bic",12,"circular")
+print("BOLIGRAFOS")
+boligrafo1=Boligrafo("rosa","bic",12,"circular")
 boligrafo1.imprimir_info()
 print('*'*15)
 boligrafo2=Boligrafo("azul","bic",12,"circular")
@@ -34,9 +36,10 @@ print('*'*15)
 boligrafo6=Boligrafo("rojo","bic",12,"circular")
 boligrafo6.imprimir_info() 
 print('*'*15)
-"""
+
 #PLUMON
-""" plumon1=Plumon("rosa","crayola",25)
+print("PLUMON")
+plumon1=Plumon("rosa","crayola",25)
 plumon1.imprimir_info()
 print('*'*15)
 plumon2=Plumon("morado","crayola",25)
@@ -44,12 +47,13 @@ plumon2.imprimir_info()
 print('*'*15)
 plumon3=Plumon("amarillo","crayola",25)
 plumon3.imprimir_info()
-print('*'*15) """
+print('*'*15)
 #TIJERAS
-""" tijera1=Tijera("azul","escolar",8)
-tijera1.imprimir_info() """
+tijera1=Tijera("azul","escolar",8)
+tijera1.imprimir_info()
 #LIBRETAS
-""" libreta1=Libreta("celeste",15,12,"cosida")
+print("LIBRETAS")
+libreta1=Libreta("celeste",15,12,"cosida")
 libreta1.mostrar_info()
 print('*'*15)
 libreta2=Libreta("rosa",30,15,"resorte")
@@ -57,9 +61,10 @@ libreta2.mostrar_info()
 print('*'*15)
 libreta3=Libreta("morada",20,15,"resorte grueso")
 libreta3.mostrar_info()
-print('*'*15) """
+print('*'*15)
 #CINTAS
-""" cinta1=Cinta("azul", 2, "1 metro")
+print("CINTAS")
+cinta1=Cinta("azul", 2, "1 metro")
 cinta1.mostrar_info()
 print('*'*15)
 cinta2=Cinta("rosa", 2, "1 metro")
@@ -70,12 +75,15 @@ cinta3.mostrar_info()
 print('*'*15)
 cinta4=Cinta("verde", 2, "1 metro")
 cinta4.mostrar_info()
-print('*'*15) """
+print('*'*15)
 #ESPEJO
-""" espejo1=Espejo("rosa","corazon",225,25)
-espejo1.mostrar_info() """
+print("ESPEJOS")
+espejo1=Espejo("rosa","corazon",225,25)
+espejo1.mostrar_info()
+print('*'*15)
 #PINTURA UÑAS
-""" esmalte1=Pintura_uñas("celeste","circular",50, 3)
+print("PINTA UÑAS")
+esmalte1=Pintura_uñas("celeste","circular",50, 3)
 esmalte1.mostrar_info()
 print('*'*15)
 esmalte2=Pintura_uñas("blanco","circular",25, 5)
@@ -95,40 +103,73 @@ esmalte6.mostrar_info()
 print('*'*15)
 esmalte7=Pintura_uñas("brillos","circular",50, 3)
 esmalte7.mostrar_info()
-print('*'*15) """
+print('*'*15)
 #PEGAMENTO
-""" pegamento1=Pegamento("rosa","cilindro", 25, "mediano",)
-pegamento1.mostrar_info() """
+print("PEGAMENTOS")
+pegamento1=Pegamento("rosa","cilindro", 25, "mediano",)
+pegamento1.mostrar_info()
 #RETRATO
-""" retrato1=Retrato("beige",25,15,"estrellas doradas")
+print("RETRATOS")
+retrato1=Retrato("beige",25,15,"estrellas doradas")
 retrato1.mostrar_info()
 print('*'*15)
 retrato2=Retrato("blanco",25,15,"flores rosas")
-retrato2.mostrar_info() """
+retrato2.mostrar_info()
 #TELEFONO
-""" telefono1=Telefono("rosa","analogico",250,"1 kilogramo")
+print("TELEFONOS")
+telefono1=Telefono("rosa","analogico",250,"1 kilogramo")
 telefono1.mostrar_info() 
-print('*'*15)"""
+print('*'*15)
 #STICKER
-""" hoja1=Hoja_sticker("gris, morado, azul", "cilindros",25)
+print("STICKERS")
+hoja1=Hoja_sticker("gris, morado, azul", "cilindros",25)
 hoja1.mostrar_info()
 print('*'*15)
 hoja2=Hoja_sticker("celeste, plateado, morado", "bolitas",25)
 hoja2.mostrar_info()
-print('*'*15) """
+print('*'*15)
 #POSTER
-""" poster1=Poster("celeste", "rectangular", "rombos")
-poster1.mostrar_info
+print("POSTERS")
+poster1=Poster("celeste", "rectangular", "rombos")
+poster1.mostrar_info()
 print('*'*15)
 poster2=Poster("morado", "rectangular", "letras amarillas")
-poster2.mostrar_info
+poster2.mostrar_info()
 print('*'*15)
 poster3=Poster("colores", "cuadrado", "foto de un arbol")
-poster3.mostrar_info
+poster3.mostrar_info()
 print('*'*15)
 poster4=Poster("colores", "rectangular", "foto de la playa")
-poster4.mostrar_info
-print('*'*15) """
+poster4.mostrar_info()
+print('*'*15)
 #CUCAS
-cuca1=Cuca("celeste", "pequeña","pinza")
+print("CUCAS")
+cuca1=Cuca("celeste", "pequeña","pinza", "con pelitos")
 cuca1.mostrar_info()
+print('*'*15)
+cuca2=Cuca("rosa", "pequeña","pinza", "con pelitos")
+cuca2.mostrar_info()
+print('*'*15)
+cuca3=Cuca("amarillo", "pequeña","pinza", "con pelitos")
+cuca3.mostrar_info()
+print('*'*15)
+cuca4=Cuca("rosa", "pequeña","pinza", "lisa")
+cuca4.mostrar_info()
+print('*'*15)
+cuca5=Cuca("morada", "pequeña","pinza", "lisa")
+cuca5.mostrar_info()
+print('*'*15)
+cuca6=Cuca("naranja", "pequeña","pinza", "lisa")
+cuca6.mostrar_info()
+print('*'*15)
+#PINTA LABIOS
+print("PINTA LABIOS")
+labial1=Pinta_Labios("rosa","largo",50,"brillos")
+labial1.mostrar_info()
+print('*'*15)
+labial1=Pinta_Labios("plateado","largo",30,"brillos")
+labial1.mostrar_info()
+print('*'*15)
+labial1=Pinta_Labios("naranja","largo",50,"opaco")
+labial1.mostrar_info()
+print('*'*15)
