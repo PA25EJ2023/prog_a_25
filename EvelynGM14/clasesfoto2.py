6810769f491ebcd2ddf42a686dc6c7baa5673fbc
Evelyn Larissa Garza Mata <evelyn.gm14@hotmail.com>
2023-05-05 04:04:24 -0600

    Agregué las clases: Celular,Camisa,Saco

--- EvelynGM14/clasesfoto2.py
+++ EvelynGM14/clasesfoto2.py
@@ -29,3 +29,36 @@ class Reloj:
     
     def info(self):
         print(f"1 RELOJ\nColor: {self.color}\nTamaño: {self.tamaño}\nPrecio: {self.material}\nMarca: {self.marca}")
+
+
+class Celular:
+    def __init__(self,color,tamaño,precio,marca):
+        self.color = color
+        self.tamaño = tamaño
+        self.precio = precio
+        self.marca = marca
+    
+    def info(self):
+        print(f"1 CELULAR\nColor: {self.color}\nTamaño: {self.tamaño}\nPrecio: {self.precio}\nMarca: {self.marca}")
+
+
+class Camisa:
+    def __init__(self,color,talla,material,marca):
+        self.color = color
+        self.talla = talla
+        self.material = material
+        self.marca = marca
+    
+    def info(self):
+        print(f"1 CAMISA\nColor: {self.color}\nTamaño: {self.talla}\nPrecio: {self.material}\nMarca: {self.marca}")
+
+
+class Saco:
+    def __init__(self,color,talla,material,marca):
+        self.color = color
+        self.talla = talla
+        self.material = material
+        self.marca = marca
+    
+    def info(self):
+        print(f"1 SACO\nColor: {self.color}\nTamaño: {self.talla}\nPrecio: {self.material}\nMarca: {self.marca}")
--- EvelynGM14/creacion_impresion_objetos_f2.py
+++ EvelynGM14/creacion_impresion_objetos_f2.py
@@ -1,4 +1,4 @@
-from clasesfoto2 import Maletin,Laptop,Reloj
+from clasesfoto2 import Maletin,Laptop,Reloj,Celular,Camisa,Saco
 
 print("*"*50)
 print("Este es el Maletin")
@@ -23,4 +23,28 @@ print("*"*50)
 reloj1 = Reloj("Negro","32mm","Acero Inoxidable","Casio")
 
 reloj1.info()
+
+print("*"*50)
+print("Este es el celular")
+print("*"*50)
+
+celular1 = Celular("Negro","5 pulgadas",12000,"Samsung")
+
+celular1.info()
+
+print("*"*50)
+print("Esta es la camisa")
+print("*"*50)
+
+camisa1 = Camisa("Blanco","M","Almidón","Svperbia")
+
+camisa1.info()
+
+print("*"*50)
+print("Este es el saco")
+print("*"*50)
+
+saco1 = Saco("Gris","M","Mezclilla","Svperbia")
+
+saco1.info()
 
