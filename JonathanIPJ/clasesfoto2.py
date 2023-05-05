015e9a8c11dcb335c889697a72ba7bb8a9f8e9fe
Jonathan Ivan Perez Jasso <ivan0410jpj@gmail.com>
2023-05-05 16:43:11 -0500

    Se agregaron las clases Zapatos, Telefono, Cargador de telefono, Tripode y Cable

--- JonathanIPJ/clasesfoto2.py
+++ JonathanIPJ/clasesfoto2.py
@@ -106,3 +106,58 @@ class Lapiz:
 
     def imprimir(self):
         print(f"Lapiz- Marca:{self.marca}, Precio:{self.precio}, Tamaño:{self.tamano}, Color:{self.color}")
+
+
+class Zapato:
+    def __init__(self, marca, precio, color, talla):
+        self.marca = marca 
+        self.precio = precio 
+        self.color = color 
+        self.talla = talla 
+
+    def imprimir(self):
+        print(f"Zapatos- Marca:{self.marca}, Precio:{self.precio}, Color:{self.color}, Talla:{self.talla}")
+
+
+class Telefono:
+    def __init__(self, marca, precio, color, modelo):
+        self.marca = marca 
+        self.precio = precio 
+        self.color = color 
+        self.modelo = modelo 
+
+    def imprimir(self):
+        print(f"Telefono- Marca:{self.marca}, Precio:{self.precio}, Color:{self.color}, Modelo:{self.modelo}")
+
+
+class Cargador_Telefono:
+    def __init__(self, marca, tipo, precio, tipo_entrada):
+        self.marca = marca 
+        self.tipo = tipo 
+        self.precio = precio 
+        self.tipo_entrada = tipo_entrada
+
+    def imprimir(self):
+        print(f"Cargador de Telefono- Marca:{self.marca}, Tipo de cargador:{self.tipo}, Precio:{self.precio}, Tipo de entrada{self.entrada}")
+
+
+class Tripode:
+    def __init__(self, color, tamano, marca, precio):
+        self.color = color 
+        self.tamano = tamano 
+        self.marca = marca 
+        self.precio = precio
+
+    def imprimir(self):
+        print(f"Tripode- Color:{self.color}, Tamaño:{self.tamano}, Marca:{self.marca}, Precio:{self.precio}")
+
+
+class Cable:
+    def __init__(self, color, marca, precio, num_camaras):
+        self.color = color 
+        self.marca = marca 
+        self.precio = precio 
+        self.num_camaras = num_camaras
+
+    def imprimir(self):
+        print(f"Cable de alimentacion- Color:{self.color}, Marca:{self.marca}, Precio:{self.precio}, Numero de camaras que se pueden conectar:{self.num_camaras}") 
