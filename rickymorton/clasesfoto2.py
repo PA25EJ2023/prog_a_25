62acbecdb4de59ed2b50ab599a6430922887acb6
Ricardo Enrique Morton Sosa <rickysosa486@outlook.com>
2023-05-05 15:01:01 -0600

    Agregamos ultimas 4 clases

--- rickymorton/clasesfoto2.py
+++ rickymorton/clasesfoto2.py
@@ -114,3 +114,50 @@ class Lente_camara:
     
     def info(self):
         print(f'marca: {self.marca}\n color: {self.color}\n tamaño: {self.tamaño}\n precio: {self.precio}')
+
+class Chaqueta:
+    def __init__(self,marca,color,talla,tipo):
+        self.marca = marca
+        self.color = color
+        self.talla = talla
+        self.tipo = tipo
+    
+    def info(self):
+        print(f'marca: {self.marca}\n color: {self.color}\n talla: {self.talla}\n tipo: {self.tipo}')
+
+class Protector_lente:
+    def __init__(self,marca,color,tamaño,precio):
+        self.marca = marca
+        self.color = color
+        self.tamaño = tamaño
+        self.precio = precio
+    
+    def info(self):
+        print(f'marca: {self.marca}\n color: {self.color}\n tamaño: {self.tamaño}\n precio: {self.precio}')
+
+class Estuche_camara:
+    def __init__(self,marca,color,tamaño):
+        self.marca = marca
+        self.color = color
+        self.tamaño = tamaño
+    
+    def info(self):
+        print(f'marca: {self.marca}\n color: {self.color}\n tamaño: {self.tamaño}')
+
+class Marcador:
+    def __init__(self,marca,color,tipo_pluma):
+        self.marca = marca
+        self.color = color
+        self.tipo_pluma = tipo_pluma
+    
+    def info(self):
+        print(f'marca: {self.marca}\n color: {self.color}\n tipo pluma: {self.tipo_pluma}')
+
+class Lapices:
+    def __init__(self,marca,color,precio):
+        self.marca = marca
+        self.color = color
+        self.precio = precio
+    
+    def info(self):
+        print(f'marca: {self.marca}\n color: {self.color}\n precio: {self.precio}')
