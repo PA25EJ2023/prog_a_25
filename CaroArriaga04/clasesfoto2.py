7844cdcb355abd94c3f51fdff95d83bcb6eca333
Carolina Arriaga Galaviz <c.arriagag03@gmail.com>
2023-05-04 23:03:52 -0600

    Correciones

--- CaroArriaga04/clasesfoto2.py
+++ CaroArriaga04/clasesfoto2.py
@@ -1,5 +1,14 @@
 class Celular:
-    pass
+    def __init__(self,color,marca,precio,almacenamiento):
+        self.color = color
+        self.marca = marca
+        self.precio = precio
+        self.alamcenamiento = almacenamiento
+    def info(self):
+        print(f"La marca del celular es: {self.marca}")
+        print(f"El precio es de: ${self.precio}")
+        print(f"Su color es: {self.color}")
+        print(f"La capacidad de almacenamiento es de: {self.almacenamiento}")
 
 class Camisa:
     pass
