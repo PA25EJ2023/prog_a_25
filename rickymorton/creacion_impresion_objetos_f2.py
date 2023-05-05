be7865758a138631a3c254ec04283600824a7758
Ricardo Enrique Morton Sosa <rickysosa486@outlook.com>
2023-05-05 12:29:22 -0600

    Agregamos Pila_lap,Camisa y Lente_camara

--- rickymorton/creacion_impresion_objetos_f2.py
+++ rickymorton/creacion_impresion_objetos_f2.py
@@ -1,5 +1,5 @@
 from clasesfoto2 import Revista,Portafolio,Zapatos,Laptop,Pila_camara,Tripie
-from clasesfoto2 import Celular,Pila_cel,Reloj
+from clasesfoto2 import Celular,Pila_cel,Reloj,Pila_lap,Camisa,Lente_camara
 
 revista1 = Revista("Juice","Para jovenes","Junio del 2015",150)
 print("Revista ")
@@ -41,3 +41,18 @@ reloj1 = Reloj("Lorus","Negro","Unisex",900)
 print("Reloj")
 reloj1.info()
 
+pilalap1 = Pila_lap("Mac","Blanco","Tipo mac",500)
+print("Pila laptop")
+pilalap1.info()
+
+camisa1 = Camisa("SVPERVIA","Blanca","G","De vestir")
+print("Camisa")
+camisa1.info()
+
+lente1 = Lente_camara("Canon","Negro","50mm F/1.8 STM",4200)
+print("Lenta para camara 1")
+lente1.info()
+
+lente2 = Lente_camara("Canon","Negro","50mm F/1.8 STM",4200)
+print("Lenta para camara 2")
+lente2.info()
