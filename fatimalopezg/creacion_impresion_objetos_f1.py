9539f1788565c5b065a3fb7a39fb7ccc1dea05fc
Fátima Alejandra López Guzmán <fatima.lopezgzm@uanl.edu.mx>
2023-05-05 01:16:48 -0600

    modifique clases

--- fatimalopezg/creacion_impresion_objetos_f1.py
+++ fatimalopezg/creacion_impresion_objetos_f1.py
@@ -1,4 +1,4 @@
-from clasesfoto1 import Esmalte
+from clasesfoto1 import Esmalte,Espejo_corazon,Retrato,Marcador,Boligrafo,Libreta,Pegamento,Cinta,Telefono,Sticker,Broche,Labial,Tijeras,Piedrita
 
 esmalte1=Esmalte("rosa pastel","cuadrado",20,"chico","5ml")
 esmalte1.info()
@@ -12,3 +12,7 @@ esmalte3.info()
 esmalte4=Esmalte("blanco","cilindro",30,"grande","12ml")
 esmalte4.info()
 
+espejo1=Espejo_corazon(10,14,"rojo con base rosa",100)
+espejo1.info()
+
+retrato1=Retrato()
