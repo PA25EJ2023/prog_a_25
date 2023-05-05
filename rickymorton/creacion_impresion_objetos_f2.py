966b3fb6f1d0f1160e3ef51a80223f4d8b0b0388
Ricardo Enrique Morton Sosa <rickysosa486@outlook.com>
2023-05-05 11:44:41 -0600

    Agregamos 3 clases mas

--- rickymorton/creacion_impresion_objetos_f2.py
+++ rickymorton/creacion_impresion_objetos_f2.py
@@ -1,4 +1,4 @@
-from clasesfoto2 import Revista,Portafolio,Zapatos
+from clasesfoto2 import Revista,Portafolio,Zapatos,Laptop,Pila_camara,Tripie
 
 revista1 = Revista("Juice","Para jovenes","Junio del 2015",150)
 print("Revista ")
@@ -11,3 +11,19 @@ portafolio1.info()
 zapatos1 = Zapatos("Lacoste","Cafe",2000)
 print("Zapatos")
 zapatos1.info()
+
+laptop1 = Laptop("MAC","Plateada",15000)
+print("Laptop")
+laptop1.info()
+
+pilacam1 = Pila_camara("King","Negra","Recargable",300)
+print("Pila 1")
+pilacam1.info()
+
+pilacam2 = Pila_camara("King","Negra","Recargable",300)
+print("Pila 2")
+pilacam2.info()
+
+tripie1 = Tripie("Excell","Negro","106cm/42",400)
+print("Tripie")
+tripie1.info()
