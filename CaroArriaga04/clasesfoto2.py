0704cb7c2f968e6b04bd825b21e3e616fa42b1cb
Carolina Arriaga Galaviz <c.arriagag03@gmail.com>
2023-05-04 23:30:08 -0600

    Cree clases laptop, revista, reloj y zapato

--- CaroArriaga04/clasesfoto2.py
+++ CaroArriaga04/clasesfoto2.py
@@ -47,16 +47,52 @@ class Cargador:
         print(f"El precio es de: ${self.precio}")
 
 class Laptop:
-    pass
+    def __init__(self,marca,tipo,color,precio):
+        self.color = color
+        self.marca = marca
+        self.tipo = tipo
+        self.precio = precio 
+    def info(self):
+        print(f"El color es: {self.color}")
+        print(f"La marca es: {self.marca}")
+        print(f"Es de tipo: {self.tipo}")
+        print(f"El precio es de: ${self.precio}")
 
 class Revista:
-    pass
+    def __init__(self,precio,titulo,editorial,idioma):
+        self.precio = precio
+        self.titulo = titulo
+        self.editorial = editorial
+        self.idioma = idioma
+    def info(self):
+        print(f"El precio es de: {self.precio}")
+        print(f"Titulo: {self.titulo}")
+        print(f"La editorial es: {self.editorial}")
+        print(f"Esta en idioma: {self.idioma}")
 
 class Reloj:
-    pass
+    def __init__(self,marca,tipo,precio,color):
+        self.marca = marca
+        self.tipo = tipo
+        self.precio = precio 
+        self.color = color
+    def info(self):
+        print(f"La marca es: {self.marca}")
+        print(f"Es de tipo: {self.tipo}")
+        print(f"El precio es de: ${self.precio}")
+        print(f"El color es: {self.color}")
 
 class Zapato:
-    pass
+    def __init__(self,material,color,estilo,precio):
+        self.material = material
+        self.color = color
+        self.estilo = estilo
+        self.precio = precio
+    def info(self):
+        print(f"El material es: {self.material}")
+        print(f"El color es: {self.color}")
+        print(f"El estilo es: {self.estilo}")
+        print(f"El precio es de: ${self.precio}")
 
 class Lapiz:
     pass
