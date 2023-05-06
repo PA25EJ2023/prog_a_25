192a6f1bb04a63a9a38a6fa4f0c313ed8bd37379
JazielCam04 <robles.jza@gmail.com>
2023-05-06 17:40:14 -0500

    Agregamos las ultimas clases y modificamos un archivo

--- JazielCam04/creacion_impresion_onjetos_f2.py
+++ JazielCam04/creacion_impresion_onjetos_f2.py
@@ -1,5 +1,5 @@
 from clasesfoto2 import Revista, Portafolio, Zapatos, Laptop, Flash, Tripie
-from clasesfoto2 import Celular, Pila, Reloj, Camisa, Lente_camara, Protector
+from clasesfoto2 import Celular, Disco_duro, Reloj, Camisa, Lente_camara, Protector
 from clasesfoto2 import Estuche_camara, Marcador, Lapices, Cargador_lap
 
 revista1 = Revista("Juice","Jovenes",80)
@@ -34,7 +34,62 @@ celular = Celular("Bmobile", "AX687", "Azul", 700)
 print("Celular ")
 celular.info()
 
-pila = Pila("Power bank","Recargable","Negro",500)
-print("Pila del Celular")
+disco_duro = Disco_duro("Lenovo","1 TB","Negro",2500)
+print("Disco Duro")
 pila.info()
 
+reloj = Reloj("Samsung","Negro",1900)
+print("Reloj")
+reloj.info()
+
+cargador = Cargador_lap("Mac","Tipo mac","Blanco",700)
+print("Cargador Laptop")
+cargador.info()
+
+camisa = Camisa("SVPERVIA","Blanca","G","De vestir")
+print("Camisa")
+camisa.info()
+
+lente1 = Lente_camara("Canon","Negro","50mm F/1.8 STM",4200)
+print("Lenta de camara 1")
+lente1.info()
+
+lente2 = Lente_camara("Canon","Negro","54mm F/1.8 STM",4400)
+print("Lenta de camara 2")
+lente2.info()
+
+protecto1 = Protector("Canon","Negro","58 mm",320)
+print("Protector 1")
+protecto1.info()
+
+protecto2 = Protector("Canon","Negro","49 mm",170)
+print("Protector 2")
+protecto2.info()
+
+protecto3 = Protector("Canon","Negro","58 mm",320)
+print("Protector 3")
+protecto3.info()
+
+marcador = Marcador("Zebra","Negro","Para caligrafía","10.05 cm")
+print("Marcador")
+marcador.info()
+
+estuche = Estuche_camara("Pixel", "Negro", "Mediano", 800)
+print("Estuche para camara")
+estuche.info()
+
+lapiz1 = Lapices("BacoColor","Negro",20,"15.5cm")
+print("Lapiz 1")
+lapiz1.info()
+
+lapiz2 = Lapices("BacoColor","Negro",25,"15.5m")
+print("Lapiz 2")
+lapiz2.info()
+
+lapiz3 = Lapices("BacoColor","Negro",30,"15.5m")
+print("Lapiz 3")
+lapiz3.info()
+
+lapiz4 = Lapices("BacoColor","Blanco",45,"15.5cm")
+print("Lapiz 4")
+lapiz4.info()
