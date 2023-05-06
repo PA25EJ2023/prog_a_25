5778f3a10f6f67a6b19126af68aeee3c6535bb7e
MiaMendozaS <jamilet.mendozas@uanl.edu.mx>
2023-05-06 05:25:22 -0600

    Se agregaron 3 clases: Libreta, Esmalte_uñas y Brillo Labial. También se modificaron las 3 clases anteriores.

--- MiaMendozaS/Clases_Foto1.py
+++ MiaMendozaS/Clases_Foto1.py
@@ -1,22 +1,22 @@
 class Boligrafo:
-    def __init__(self,tamaño,color,marca,precio):
-        self.tamaño= tamaño
+    def __init__(self,color,tamaño,marca,precio):
         self.color= color
+        self.tamaño= tamaño
         self.marca= marca
         self.precio= precio 
 
     def mostrar_info(self):
-        print(f"Tamaño: {self.tamaño} \nColor: {self.color} \nMarca: {self.marca} \nPrecio: {self.precio}")
+        print(f"Color: {self.color}\nTamaño: {self.tamaño}\nMarca: {self.marca} \nPrecio: {self.precio}")
 
 class Marcador:
-    def __init__(self,tamaño,color,marca,precio):
-        self.tamaño= tamaño
+    def __init__(self,color,tamaño,marca,precio):
         self.color= color
+        self.tamaño= tamaño
         self.marca= marca
         self.precio= precio 
 
     def mostrar_info(self):
-        print(f"Tamaño: {self.tamaño} \nColor: {self.color} \nMarca: {self.marca} \nPrecio: {self.precio}")
+        print(f"Color: {self.color}\nTamaño: {self.tamaño}\nMarca: {self.marca}\nPrecio: {self.precio}")
     
 class Cinta:
     def __init__(self,color,longitud,ancho,marca):
@@ -28,6 +28,35 @@ class Cinta:
     def mostrar_info(self):
         print(f"Color: {self.color}\nLongitud: {self.longitud}\nAncho: {self.ancho}\nMarca: {self.marca}")
 
+class Libreta:
+    def __init__(self,color,tamaño,num_hojas,tipo):
+        self.color= color
+        self.tamaño= tamaño
+        self.num_hojas= num_hojas
+        self.tipo= tipo
+    
+    def mostrar_info(self):
+        print(f"Color: {self.color}\nTamaño: {self.tamaño}\nNúmero de Hojas: {self.num_hojas}\nTipo: {self.tipo}")
+
+class Esmalte_Uñas:
+    def __init__(self,color,contenido,forma,marca):
+        self.color= color
+        self.contenido= contenido
+        self.forma= forma
+        self.marca= marca
+    
+    def mostrar_info(self):
+        print(f"Color: {self.color}\nContenido Neto: {self.contenido}\nForma: {self.forma}\nMarca: {self.marca}")
+
+class Brillo_labial:
+    def __init__(self,color,contenido,forma,marca):
+        self.color= color
+        self.contenido= contenido
+        self.forma= forma
+        self.marca= marca
+    
+    def mostrar_info(self):
+        print(f"Color: {self.color}\nContenido Neto: {self.contenido}\nForma: {self.forma}\nMarca: {self.marca}")
 
 
 
