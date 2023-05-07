5c85b49bf06a5991827cc3b2302f430fa6545c16
valeodem <valentina.mendozao@uanl.edu.mx>
2023-05-06 20:17:36 -0500

    imprimimos los objetos segunda foto

---
+++ valeodem/impresion_obj_foto2.py
@@ -0,0 +1,93 @@
+from clases_ev2_foto2 import: *
+
+print("--Computadora--")
+computadora = Computadora("Apple","22 x 17 cm","Blanco","1MB",28.000)
+computadora.info()
+
+print("****************************")
+print("--Maletin--")
+maletin = Maletin("Cafe","Vintage","Mediano",1.000)
+maletin.info()
+
+print("****************************")
+print("--Lentes de camara 1--")
+lente1 = Lente_camara("Cannon","Negro","x40",3.000)
+lente1.info()
+
+print("--Lentes de camara 2--")
+lente2 = Lente_camara("Cannon","Negro","x30",2.500)
+lente2.info()
+
+print("****************************")
+print("--Revista--")
+revista = Revista("Juice","The Chainsmokers","Supermercados",60)
+revista.info()
+
+print("****************************")
+print("--Zapatos--")
+zapatos = Zapatos("Cafe","Vintage","28 1/2",500)
+zapatos.info()
+
+print("****************************")
+print("--Cargador 1--")
+cargador1 = Cargador("Apple","Blanco","1m","Computadora",1.000)
+cargador1.info()
+
+print("--Cargador 2--")
+cargador2 = Cargador("Cannon","Blanco","30cm","Camara",1.000)
+cargador2.info()
+
+print("****************************")
+print("--Reloj--")
+reloj = Reloj("Negro","Rolex","Digital",7.000)
+reloj.info()
+
+print("****************************")
+print("--Pila de camara 1--")
+pila1 = Pila_camara("Negro","Cannon","Digital",1.000)
+pila1.info()
+
+print("--Pila de camara 2--")
+pila2 = Pila_camara("Negro","Cannon","Digital",1.000)
+pila2.info()
+
+print("****************************")
+print("--Celular--")
+celular = Celular("Negro","Apple",                                        "   ",7.000)
+celular.info()
+
+print("****************************")
+print("--Camisa 1--")
+camisa1 = Camisas("Blanco","H&M","Mediana",500)
+camisa1.info()
+
+print("--Camisa 2--")
+camisa2 = Camisas("Negro deslavado","H&M","Mediana",600)
+camisa2.info()
+
+print("****************************")
+print("--Lapiz 1--")
+lapiz1 = Lapices("Negro","BIC","Chico",20)
+lapiz1.info()
+
+print("--Lapiz 2--")
+lapiz2 = Lapices("Negro","BIC","Chico",20)
+lapiz2.info()
+
+print("--Lapiz 3--")
+lapiz3 = Lapices("Grafito","BIC","Chico",20)
+lapiz3.info()
+
+print("--Lapiz 4--")
+lapiz4 = Lapices("Blanco","BIC","Chico",20)
+lapiz4.info()
+
+print("****************************")
+print("--Marcador--")
+marcador = Marcador("Negro","Sharpee","17 cm",35)
+marcador.info()
+
+print("****************************")
+print("--Tripie--")
+tripie = Tripie("Negro","Amazon","1 m",650)
+tripie.info()
