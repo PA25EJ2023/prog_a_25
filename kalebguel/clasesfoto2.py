dd309ae94561cbe3f1760be661d63c255d8b1623
Osmar Kaleb Guel Orozco <osmar.guelo@uanl.edu.mx>
2023-05-07 20:25:00 -0600

    clase tripie,revista,pilas camara,maletin,lapiz

--- kalebguel/clasesfoto2.py
+++ kalebguel/clasesfoto2.py
@@ -18,7 +18,7 @@ class Celular:
     def imprimir(self):
         print(f'color:{self.color},altura:{self.altura},ancho:{self.ancho},tipo:{self.tipo}')
 
-class Zapatos:
+class Zapato:
     def __init__(self,color,marca,talla,tipo):
         self.color = color
         self.marca = marca
@@ -37,5 +37,53 @@ class Reloj:
 
     def imprimir(self):
         print(f'marca:{self.marca},color:{self.color},tipo:{self.tipo},forma:{self.forma}')
+
+class Tripie:
+    def __init__(self,color,altura,marca):
+        self.color = color
+        self.altura = altura
+        self.marca = marca
+
+    def imprimir(self):
+        print(f'color:{self.color},altura:{self.altura},marca:{self.marca}')
+
+class Revista:
+    def __init__(self,altura,ancho,nombre):
+        self.altura = altura
+        self.ancho = ancho
+        self.nombre = nombre
+
+    def imprimir(self):
+        print(f'altura:{self.altura},ancho:{self.ancho},nombre:{self.nombre}') 
+
+class Pilas_camara:
+    def __init__(self,marca,tamaño,altura,color):
+        self.marca = marca
+        self.tamaño = tamaño
+        self.altura = altura
+        self.color = color
+
+    def imprimir(self):
+        print(f'marca:{self.marca},tamaño:{self.tamaño},altura:{self.altura},color:{self.color}')    
+
+class Maletin:
+    def __init__(self,color,tamaño,tipo,ancho):
+        self.color = color
+        self.tamaño = tamaño
+        self.tipo = tipo
+        self.ancho = ancho
+
+    def imprimir(self):
+        print(f'color:{self.color},tamaño:{self.tamño},tipo:{self.tipo},ancho:{self.ancho}')
+
+class Lapiz:
+    def __init__(self,color,marca,altura,ancho):
+        self.color = color 
+        self.marca = marca
+        self.altura = altura
+        self.ancho = ancho
+
+    def imprimir(self):
+        print(f'color:{self.color},marca:{self.marca},altura:{self.altura},ancho:{self.ancho}') 
         
-            
+           
