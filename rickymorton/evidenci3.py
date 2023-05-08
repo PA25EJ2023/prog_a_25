56fc97146a642f7e9820264d47009515cfda13b6
Ricardo Enrique Morton Sosa <rickysosa486@outlook.com>
2023-05-08 16:20:17 -0600

    Agregamos menu de opciones

---
+++ rickymorton/evidenci3.py
@@ -0,0 +1,9 @@
+
+alumnos = []
+
+print("Bienvenido al programa")
+op= 0
+while op != 5:
+    print("Opciones disponibles")
+    print("1.-Agregar alumno\n 2.-Mostrar info alumno\n 3.-Eliminar alumno\n 4.-Mostrar todos los alumnos\n 5.-Salir")
+    op = int(input("ingresa una opcion"))
