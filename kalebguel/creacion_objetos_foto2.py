7d078a882c1a8f0b554df40eaeb44670db0a9959
Osmar Kaleb Guel Orozco <osmar.guelo@uanl.edu.mx>
2023-05-07 22:48:36 -0600

    creando objetos foto2

---
+++ kalebguel/creacion_objetos_foto2.py
@@ -0,0 +1,42 @@
+from clasesfoto2 import Camisa,Celular,Zapato,Reloj,Tripie,Revista,Pilas,Maletin,Lapiz
+from clasesfoto2 import Laptop,Cargador,Lente_camara,Tapa_camara,Estuche_camara,Chaqueta
+
+#creando objetos foto2
+print("Camisas")
+Camisa1=Camisa("Blanca","SHEIN","1500","Manga larga")
+
+Camisa1.imprimir()
+
+print("celulares")
+Celular1=Celular("Negro","8cm","6cm","touch")
+
+Celular1.imprimir()
+
+print("Zapatos")
+Zapatos1=Zapato("Cafes","ZARA","7.5","Vestir")
+Zapatos1.imprimir()
+
+print("Relojes")
+Reloj1=Reloj("Casio","Negro","Touch","Circular")
+Reloj1.imprimir()
+
+print("Tripies")
+Tripie1=Tripie("Negro","15cm","Cannon")
+Tripie1.imprimir()
+
+print("Revistas")
+Revista1=Revista("10cm","9cm","Juice")
+Revista1.imprimir()
+
+print("Pilas")
+Pila1=Pilas("Cannon","Medianas","7cm","Negra","Camara")
+Pila2=Pilas("Cannon","Medianas","7cm","Negra","Camara")
+Pila3=Pilas("Sony","Grandes","10cm","Negra","Celular")
+
+Pila1.imprimir()
+Pila2.imprimir()
+Pila3.imprimir()
+
+
+
+
