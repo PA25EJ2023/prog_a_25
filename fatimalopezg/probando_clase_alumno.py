2c292bbb96dcd359b844619289635914b73c24b7
Fátima Alejandra López Guzmán <fatima.lopezgzm@uanl.edu.mx>
2023-05-09 23:58:53 -0600

    cree la primera opcion del menu

---
+++ fatimalopezg/probando_clase_alumno.py
@@ -0,0 +1,31 @@
+from clase_alumno import Alumno 
+calificaciones=[] #crear lista vacia 
+alumnos=[] 
+
+ 
+print(" -- MENU DE OPCIONES FACPYA --") 
+while True: 
+    print("1. Agregar alumno") 
+    print("2. Mostrar info del alumno") 
+    print("3. Eliminar alumno: ") 
+    print("4. Mostrar todos los alumnos: ") 
+    print("5. Salir") 
+    opcion=int(input("Bienvenido(a) ¿qué opcion desea realizar? ")) 
+    if opcion==1: 
+        mat=int(input("Ingrese la matricula del alumno: ")) #CREAR UN ALUMNO Y DE AHI MANDAR LAS CALIFICACIONES PARA LA LISTA 
+        nom=input("Ingrese el nombre del alumno: ") 
+        carr=input("Ingrese la carrera: ") 
+        cal=int(input("Ingrese la calificacion: "))  
+        #while True:  
+            #calificaciones.append(objeto) 
+            
+        objeto=Alumno(mat,nom,carr,cal) #calificaciones.append(objeto) 
+        alumnos.append(objeto) 
+        
+        
+
+ 
+
+ 
+
+ 
