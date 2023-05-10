713a335e573d87ee0970a4a722dab7a16c1041c6
Fátima Alejandra López Guzmán <fatima.lopezgzm@uanl.edu.mx>
2023-05-10 00:28:30 -0600

    opcion 5 sale del menu y ya no pregunta

--- fatimalopezg/probando_clase_alumno.py
+++ fatimalopezg/probando_clase_alumno.py
@@ -11,6 +11,7 @@ while True:
     print("4. Mostrar todos los alumnos: ") 
     print("5. Salir") 
     opcion=int(input("Bienvenido(a) ¿qué opcion desea realizar? ")) 
+    
     if opcion==1: 
         mat=int(input("Ingrese la matricula del alumno: ")) #CREAR UN ALUMNO Y DE AHI MANDAR LAS CALIFICACIONES PARA LA LISTA 
         nom=input("Ingrese el nombre del alumno: ") 
@@ -28,14 +29,22 @@ while True:
             if objeto.nombre==nom: 
                 objeto.imprimir_info() 
 
-    elif opcion==3: 
-        mat=int(input("Ingrese la matricula del alumno que desea eliminar: ")) 
-        if matricula==mat: 
-            objeto-=Alumno(mat,nom,carr,cal)  
+    #elif opcion==3: 
+        #mat=int(input("Ingrese la matricula del alumno que desea eliminar: ")) 
+        #if matricula==mat: 
+            #objeto-=Alumno(mat,nom,carr,cal)  
 
     elif opcion==4: 
         for objeto in alumnos: 
             objeto.imprimir_info() 
+
+    elif opcion==5: 
+        print("Has salido del menu") 
+        break
+        
+    
+    else: 
+        print("Intente nuevamente, la opcion agregada no existe") 
             
 
 
