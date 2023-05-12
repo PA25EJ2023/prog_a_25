810c180c0a3798cfd4a50cb0f1fafae295d8f579
Evelyn Larissa Garza Mata <evelyn.gm14@hotmail.com>
2023-05-12 03:19:11 -0600

    Agregué los últimos detalles

--- EvelynGM14/evidencia_3_objetos.py
+++ EvelynGM14/evidencia_3_objetos.py
@@ -4,7 +4,12 @@ opcion = 1
 
 lista_alumnos = []
 while opcion ==1:
-    respuesta = int(input("1.-Agregar Alumno\n2.-Mostrar info del alumno\n3.-Eliminar alumno\n4.-Mostrar todos los alumnos\n5.-Salir\nElija una opcion: "))
+    print("-"*30)
+    print("             MENÚ             ")
+    print("-"*30)
+    print("1.-Agregar Alumno\n2.-Mostrar info del alumno\n3.-Eliminar alumno\n4.-Mostrar todos los alumnos\n5.-Salir")
+    print("-"*30)
+    respuesta = int(input("Elija una opcion: "))
     if respuesta == 1:
         calificaciones = []
         matricula = int(input("Digite su matricula: "))
@@ -16,15 +21,21 @@ while opcion ==1:
                 calificaciones.append(calificacion)
         alumno = Alumno(matricula,nombre,carrera,calificaciones)
         lista_alumnos.append(alumno)
+        print("")
         print("ALUMNO AGREGADO!")
+        print("")
 
     elif respuesta == 2:
         info_alumno = int(input("Digite la matricula del alumno: "))
         if alumno.matricula == info_alumno:
+            print("*"*40)
             print(f"INFORMACIÓN DEL ALUMNO CON MATRICULA: {info_alumno}")
             alumno.imprimir_info()
+            print("*"*40)
         else:
+            print("")
             print("ERROR EN LA MATRICULA O NO EXISTE!")
+            print("")
     
 
     elif respuesta == 3:
@@ -32,24 +43,37 @@ while opcion ==1:
         for alumno in lista_alumnos:
             if alumno.matricula == matricula:
                 lista_alumnos.remove(alumno)
+                print("*"*25)
                 print("ALUMNO ELIMINADO")
+                print("*"*25)
                 break
         else:
+            print("")
             print("ERROR EN LA MATRICULA O NO EXISTE!")
+            print("")
 
     elif respuesta == 4:
-        print("---REGISTRO DE ALUMNOS---")
         if len(lista_alumnos) > 0:
+            print("")
+            print("-"*41)
+            print("            REGISTRO DE ALUMNOS            ")
+            print("-"*41)
             for alumno in lista_alumnos:
                 alumno.imprimir_info()
-                print("*"*25)
+                print("*"*40)
+            print("-"*40)
+            print("")
         else:
+            print("")
             print("NO SE ENCONTRARON ALUMNOS REGISTRADOS!")
+            print("")
 
     elif respuesta == 5:
         break
     else:
+        print("")
         print("OPCIÓN INVÁLIDA!")
+        print("")
          
 
              
