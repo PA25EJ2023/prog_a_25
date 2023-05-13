a5433a9abe720601fc068ce22b14337f44a6cb54
Fátima Alejandra López Guzmán <fatima.lopezgzm@uanl.edu.mx>
2023-05-12 19:52:19 -0600

    solo modifique una linea de la clase, ya ev3 terminada

--- fatimalopezg/clase_alumno.py
+++ fatimalopezg/clase_alumno.py
@@ -8,12 +8,11 @@ class Alumno:
 
     def calcular_promedio(self):
         prom=sum(self.calificaciones)/len(self.calificaciones)
-        return prom
-        #return prom #usar len() para sacar el total 
+        return prom #usar len() para sacar el total 
         
     def imprimir_info(self): 
         print(f"Matricula: {self.matricula}") 
         print(f"Nombre: {self.nombre}") #AQUI HACER UNA LIST, USAR CICLO HAY UNA FUNCION Q SUMA LAS CALIFICACIONES 
         print(f"Carrera: {self.carrera}") 
-        print(f"Calificaciones: {self.calificaciones}")  #print(f"Promedio: {sum(self.calificaciones)/len(self.calificaciones)}") 
+        print(f"Calificaciones: {self.calificaciones}") #print(f"Promedio: {sum(self.calificaciones)/len(self.calificaciones)}") 
         print(f"Promedio: {self.calcular_promedio()}")
