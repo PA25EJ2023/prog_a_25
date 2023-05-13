e55b755b41b3a951d6f4643205a482627971cd85
valeodem <valentina.mendozao@uanl.edu.mx>
2023-05-12 23:13:11 -0500

    alumnos

--- valeodem/Evidencia3.py
+++ valeodem/Evidencia3.py
@@ -6,8 +6,9 @@ class Alumno:
         self.calificacion = calificacion
 
     def imprimir_info(self):
-        print(f'Nombre: {self.nombre}\n Matricula: {self.matricula}\n Calificaciones: {self.calificacion}\n Carrera: {self.carrera} ')
+        print(f'Nombre: {self.nombre}\n Matricula: {self.matricula}\n Carrera: {self.carrera}\n Calificaciones: {self.calificacion}')
 
     def calcular_promedio(self):
         return sum(self.calificacion) / len(self.calificacion)
-    
+
+        
