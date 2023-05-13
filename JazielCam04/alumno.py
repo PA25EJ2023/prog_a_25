b0e58b6472e0d4d611863969f1b616d29f50895a
JazielCam04 <robles.jza@gmail.com>
2023-05-12 20:50:12 -0600

    Creacion de agregar_alumno

--- JazielCam04/alumno.py
+++ JazielCam04/alumno.py
@@ -5,16 +5,32 @@ class Alumno:
         self.carrera = carrera
         self.calificaciones = calificaciones
 
-    def imprimir_info(self):
+    def info(self):
         promedio = self.calcular_promedio()
-        print("Matrícula:", self.matricula)
-        print("Nombre:", self.nombre)
-        print("Carrera:", self.carrera)
-        print("Calificaciones:", self.calificaciones)
+        print("Tu matrícula:", self.matricula)
+        print("Tu nombre:", self.nombre)
+        print("Esta es tu carrera:", self.carrera)
+        print("Estas son tus calificaciones:", self.calificaciones)
         print("Promedio:", promedio)
 
     def calcular_promedio(self):
         total_calificaciones = sum(self.calificaciones)
         promedio = total_calificaciones / len(self.calificaciones)
         return promedio
+    
+alumnos = []
+
+def agregar_alumno():
+    matricula = input("Dime la matricula del estudiante: ")
+    nombre = input("Dime el nombre del estudiante: ")
+    carrera = input("Dime el nombre de la carrera del estudiante: ")
+    
+    calificaciones = []
+    can = int(input("Dime el numero de calificaciones: "))
+    for i in range(can):
+        calificaciones = float(input("Dime la calificacion: ".format(i+1)))
+        calificaciones.append(calificaciones)
+    alumno = Alumno(matricula, nombre, carrera, calificaciones)
+    alumnos.append(alumno)
+    print("Alumno agregado con exito.")
     
