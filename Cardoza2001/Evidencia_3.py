a15a26b643c489fb7fef0d1763507c7767b9d0df
Cardoza2001 <jose.aguirreca@uanl.edu.mx>
2023-05-12 22:45:31 -0600

    Evidencia 3 version final ultra god

--- Cardoza2001/Evidencia_3.py
+++ Cardoza2001/Evidencia_3.py
@@ -1,15 +1,56 @@
-from Registro_Alumnos import Alumno
+from Alumnos import Alumno
 
+lista =[]
 
+#Metodo para registrar alumnos
+def Registro ():
 
-lista=[]
+    nomb = input("Ingrese el nombre del alumno: ")
 
+    matri = input("Ingrese la matricula (limite 7 caracteres numericos): ")
 
+    carr = input("Ingrese la carrera del alumno: ")
+
+    calf = float(input("Ingrese la calificación del alumno: "))
+
+    dude= Alumno(nomb,matri,carr,calf)
+    lista.append(dude)
+
+#Metodo para mostrar todos los datos del alumno incluyendo su promedio 
 def Imprimir_info():
-    
-    W = 0
-    while W < len(lista[W].nom," ", lista[W].matri," ", lista[W].calf):
-        W= W + 1
+        
+    for dude in lista:
+        print("Nombre: {}, Matricula: {},Carrera: {}, Calificación: {}".format(dude.nomb,dude.matri,dude.carr,dude.calf))
+      
+        
+#Metodo para calcular el promedio
+def calcular_promedio ():
+
+    promedio = sum(dude.calf for dude in lista)/ len(lista)
+
+    print ("El promedio general de todos los alumnos es: {:.2f}".format(promedio))
+
+#Metodo para borrar a un alumno en especifico
+def Eliminar_alumno ():
+    matricula = input("Para buscar un alumno en especifico ingresa su matricula (Solo 7 caracteres numericos): ")
+
+    for f, dude in enumerate(lista):
+        if dude.matri == matricula:
+            del lista[f]
+            print ("Datos de alumno eliminados con exito")
+            return
+        print ("No se encontró a ningun alumno con esa matricula")
+        
+
+#Metodo para buscar a un alumno en especifico
+def Buscar_Alumno ():
+    matricula = input("Para buscar un alumno en especifico ingresa su matricula (Solo 7 caracteres numericos) ")
+    for dude in lista:
+        if dude.matri == matricula:
+            print("Nombre: {}, Matricula: {},Carrera: {}, Calificación: {}".format(dude.nomb,dude.matri,dude.carr,dude.calf))
+            return
+        print ("No se encontró a ningun alumno con esa matricula")
+            
 
 xd =0 
 while xd == 0:
@@ -17,34 +58,35 @@ while xd == 0:
     print ("Bienvenido al portal escolar, elija la opción que desea ejecutar.")
 
 
-    opcion= int(input("1.- Agregar Alumno \n2.- Mostrar info del alumno\n3.- Eliminar alumno\n4.- Mostrar todos los alumnos \n5.- Salir\n"))
+    opcion= int(input("1.- Agregar Alumno \n2.- Mostrar info del alumno\n3.- Eliminar alumno\n4.- Mostrar todos los alumnos \n5.- Salir\n\nIngresa una opción: "))
 
 
     if opcion == 1:
 
-        n = input("Ingrese el nombre del alumno:\n")
-
-        m = input("Ingrese la matricula (limite 7 caracteres numericos)\n")
-
-        c = input("Ingrese la calificación del alumno:\n")
-
-        objeto= Alumno(n,m,c)
-        lista.append(objeto)
+        Registro()
 
         print("Alumno registrado exitosamente")
 
     elif opcion == 2:
-        print("Alumnos registrados en el sistema:")
-        Imprimir_info()
+        Buscar_Alumno ()
 
 
     elif opcion == 3:
-        pass
-
-
+        Eliminar_alumno()
+        
+ 
     elif opcion == 4:
-        pass
+        
+        #metodos solicitados
+        Imprimir_info()
+
+        calcular_promedio()
 
 
+        
     elif opcion == 5:
         exit(print("Gracias por usar el portal escolar :)"))
+
+
+    else:
+        print ("Opción invalida eliga una opción valida")
